78d1d449dda0cd908359bba2b161ff1fa49b2d89
S. A. N. K. Bandara <mrsanbandaraprofessional@gmail.com>
2026-02-22 15:08:10 +0530

    feat: Add `encrypt_file` function to encrypt files using AESGCM.

--- aes_handle.py
+++ aes_handle.py
@@ -14,3 +14,18 @@ def load_key(key_path="secret.key"):
 
     with open(key_path, "rb") as key_file:
         return key_file.read()
+
+def encrypt_file(file_path, key):
+    aes_object = AESGCM(key)
+    nonce = os.urandom(12)
+
+    with open(file_path, "rb") as file:
+        plaintext = file.read()
+
+    cipertext = aes_object.encrypt(nonce, plaintext, None)
+    encrypt_file_path = file_path + ".env"
+
+    with open(encrypt_file_path, "wb") as file:
+        file.write(nonce + cipertext)
+
+    print(f"[+] Successfully encrypted '{file_path}' to '{encrypted_file_path}'")
